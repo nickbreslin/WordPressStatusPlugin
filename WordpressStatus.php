a506ddb5136f584941c2247b8ef9fffcda78f99e
Nick Breslin <nickbreslin@gmail.com>
2016-09-25 12:43:08 -0700

    Updating comments/meta

---
+++ WordpressStatus.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name:  Wordpress Status
+Plugin URI:   https://github.com/nickbreslin/WordpressStatusPlugin
+Description:  ...
+Version:      1.0.0
+Author:       Nick Breslin
+Author URI:   http://www.nickbreslin.com
+License:      MIT License
+*/
+
+defined( 'ABSPATH' ) or die();
