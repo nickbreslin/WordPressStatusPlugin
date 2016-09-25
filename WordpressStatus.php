c5471c93632f71ff10fe7301129700dff91e3bbe
Nick Breslin <nickbreslin@gmail.com>
2016-09-25 12:46:22 -0700

    Updating to WP naming scheme

--- WordpressStatus.php
+++ WordpressStatus.php
@@ -1,7 +1,7 @@
 <?php
 /*
-Plugin Name:  Wordpress Status
-Plugin URI:   https://github.com/nickbreslin/WordpressStatusPlugin
+Plugin Name:  WordPress Status
+Plugin URI:   https://github.com/nickbreslin/WordPressStatusPlugin
 Description:  ...
 Version:      1.0.0
 Author:       Nick Breslin
@@ -9,4 +9,11 @@ Author URI:   http://www.nickbreslin.com
 License:      MIT License
 */
 
+// Prevent direct access
 defined( 'ABSPATH' ) or die();
+
+class WordPressStatus {
+
+}
+
+$wordPressStatus = new WordPressstatus();
